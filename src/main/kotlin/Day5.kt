f6b46b63dba1076017e178ddc5c7ec9805028474
Ruan Moolman <moolman.ruan@gmail.com>
2022-12-05 16:41:44 +0200

    more list cleanup

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,7 +1,7 @@
 
 
 typealias Crate = Char
-typealias Stack = ArrayList<Crate>
+typealias Stack = MutableList<Crate>
 data class Operation(val count: Int, val from: Int, val to: Int)
 
 fun String.toOperation(): Operation {
@@ -9,7 +9,7 @@ fun String.toOperation(): Operation {
     return Operation(parts[0], parts[1] - 1, parts[2] - 1)
 }
 
-fun parse(lines: List<String>): Pair<ArrayList<Stack>, ArrayList<Operation>> {
+fun parse(lines: List<String>): Pair<List<Stack>, List<Operation>> {
     var splitLineIndex = 0
     for (i in lines.indices) {
         if (lines[i].isEmpty()) {
@@ -19,32 +19,40 @@ fun parse(lines: List<String>): Pair<ArrayList<Stack>, ArrayList<Operation>> {
     }
 
     val numStacks = lines[splitLineIndex - 1].trim().split("""\s+""".toRegex()).size
-    val stacks = ArrayList(List(numStacks) { Stack() })
+    val stacks: List<Stack> = List(numStacks) { ArrayList() }
     for (i in splitLineIndex - 2 downTo 0) {
         lines[i].windowed(3, 4)
             .map { it[1] }
             .withIndex()
             .forEach { if (it.value != ' ') stacks[it.index].add(it.value) }
     }
-
     return Pair(
         stacks,
-        ArrayList(lines.subList(splitLineIndex + 1, lines.size).map { it.toOperation() })
+        lines.subList(splitLineIndex + 1, lines.size).map { it.toOperation() }
     )
+}
+
+fun process1(stacks: List<Stack>, ops: List<Operation>): List<Stack> {
+    for (op in ops) {
+        for (i in 0 until op.count) {
+            stacks[op.to].add(stacks[op.from].last())
+            stacks[op.from].removeLast()
+        }
+    }
+    return stacks
+}
+
+fun process2(stacks: List<Stack>, ops: List<Operation>): List<Stack> {
+    val ss = stacks.toMutableList()
+    for (op in ops) {
+        ss[op.to].addAll(ss[op.from].takeLast(op.count))
+        ss[op.from] = ss[op.from].dropLast(op.count).toMutableList()
+    }
+    return ss
 }
 
 fun day5(lines: List<String>): String {
     val (stacks, ops) = parse(lines)
-
-    for (op in ops) {
-        // Part 1
-//        for (i in 0 until op.count) {
-//            stacks[op.to].add(stacks[op.from].last())
-//            stacks[op.from].removeLast()
-//        }
-        // Part 2
-        stacks[op.to].addAll(stacks[op.from].takeLast(op.count))
-        stacks[op.from] = Stack(stacks[op.from].dropLast(op.count))
-    }
-    return stacks.fold("") { a, v -> a + v.last() }
+    val ss = process2(stacks, ops)
+    return ss.fold("") { a, v -> a + v.last() }
 }
