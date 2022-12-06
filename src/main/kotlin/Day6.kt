57260a052a168146799ce180a49bf0eaeb959126
Ruan Moolman <moolman.ruan@gmail.com>
2022-12-06 07:44:42 +0200

    Day 6

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,3 +1,26 @@
+
+fun different(code: String): Boolean {
+    for (ci in 0 until code.length - 1) {
+        if (code[ci] in code.substring(ci + 1, code.length)) {
+            return false
+        }
+    }
+    return true
+}
+
+fun process(code: String, size: Int): Int {
+    val index = code
+        .windowed(size, 1)
+        .map { different(it) }
+        .indexOf(true)
+    if (index < 0) {
+        return index
+    }
+    return index + size
+}
+
 fun day6(input: String): String {
-    return ""
+    val codes = input.split("\n")
+    println("Part one: $ANSI_BLUE${codes.map { process(it, 4) }.joinToString("\n")}$ANSI_RESET")
+    return codes.map { process(it, 14) }.joinToString("\n")
 }
