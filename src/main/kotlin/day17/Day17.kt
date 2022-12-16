4343dca8213f9b4fea74de6d63e030a442c26fc4
Ruan Moolman <moolman.ruan@gmail.com>
2022-12-16 21:23:41 +0200

    Day 17: Preparation

---
+++ src/main/kotlin/day17/Day17.kt
@@ -0,0 +1,7 @@
+package day17
+
+fun run(input: String, stage: String): String {
+    println(input)
+    println(stage)
+    return ""
+}
---
+++ src/main/kotlin/day17/Day17Test.kt
@@ -0,0 +1,11 @@
+package day17
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class Day17Test {
+    @Test
+    fun simple() {
+        assertEquals(1, 1)
+    }
+}
