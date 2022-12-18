daedf7aaaf9a1a96ccfceb828228972f7eaf3bfd
Ruan Moolman <moolman.ruan@gmail.com>
2022-12-18 07:10:16 +0200

    Day 18: Part 1

--- src/main/kotlin/day18/Day18.kt
+++ src/main/kotlin/day18/Day18.kt
@@ -1,5 +1,37 @@
 package day18
 
+import output.printAnswer
+
+data class Vec3(val x: Int, val y: Int, val z: Int)
+
+operator fun Vec3.plus(other: Vec3): Vec3 {
+    return Vec3(x + other.x, y + other.y, z + other.z)
+}
+
+fun String.toVec3(): Vec3 {
+    val p = this.split(",")
+    return Vec3(p[0].toInt(), p[1].toInt(), p[2].toInt())
+}
+
 fun run(input: String, stage: String): String {
+    part1(input, stage)
     return ""
+}
+
+fun part1(input: String, stage: String) {
+    val drops = input.split("\n").map(String::toVec3).toSet()
+
+    val ans = drops.fold(0) { count, drop ->
+        count + listOf(
+            Vec3(1, 0, 0),
+            Vec3(-1, 0, 0),
+            Vec3(0, 1, 0),
+            Vec3(0, -1, 0),
+            Vec3(0, 0, 1),
+            Vec3(0, 0, -1)
+        ).filter {
+            drop + it !in drops
+        }.count()
+    }
+    printAnswer(ans, 64)
 }
