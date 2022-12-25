f8c923f0208dce5d70f19c4166fb3ab8ec9d2d2e
Ruan Moolman <moolman.ruan@gmail.com>
2022-12-25 08:45:47 +0200

    Day 25: Part 1

--- src/main/kotlin/day25/Day25.kt
+++ src/main/kotlin/day25/Day25.kt
@@ -6,7 +6,39 @@ fun run(input: String, stage: String) {
     part1(input, stage)
 }
 
+fun snafuToDecimal(input: String): Long {
+    var total = 0L
+    var base = 1L
+    for (v in input.windowed(1).reversed()) {
+        total += when (v) {
+            "2" -> 2 * base
+            "1" -> base
+            "0" -> 0
+            "-" -> -base
+            "=" -> -2 * base
+            else -> throw Exception("Invalid number")
+        }
+        base *= 5
+    }
+    return total
+}
+
+const val base = 5
+const val halfBase = base / 2
+fun decimalToSnafu(input: Long): String {
+    var rem = input
+    var res = ""
+    while (rem != 0L) {
+        val m = (rem + halfBase).mod(base)
+        res = "=-012"[m] + res
+        rem -= (m - halfBase)
+        rem /= base
+    }
+    return res
+}
+
 fun part1(input: String, stage: String) {
-    val want = if (stage == "problem") null else null
-    printAnswer(0, want, "Part 1")
+    val sumAns = input.split("\n").map(::snafuToDecimal).sum()
+    val want = if (stage == "problem") "2011-=2=-1020-1===-1" else "2=-1=0"
+    printAnswer(decimalToSnafu(sumAns), want, "Part 1")
 }
--- src/main/kotlin/day25/Day25Test.kt
+++ src/main/kotlin/day25/Day25Test.kt
@@ -5,7 +5,22 @@ import org.junit.jupiter.api.Test
 
 class Day25Test {
     @Test
-    fun one() {
-        assertEquals(1, 1)
+    fun toDecimal() {
+        assertEquals("2", decimalToSnafu(2))
+        assertEquals("=", decimalToSnafu(-2))
+        assertEquals("1=", decimalToSnafu(3))
+        assertEquals("12", decimalToSnafu(7))
+        assertEquals("2=", decimalToSnafu(8))
+        assertEquals("2-", decimalToSnafu(9))
+        assertEquals("20", decimalToSnafu(10))
+        assertEquals("21", decimalToSnafu(11))
+        assertEquals("22", decimalToSnafu(12))
+        assertEquals("1==", decimalToSnafu(13))
+        assertEquals("-2", decimalToSnafu(-3))
+        assertEquals("=0", decimalToSnafu(-10))
+        assertEquals("==", decimalToSnafu(-12))
+        assertEquals("-22", decimalToSnafu(-13))
+        assertEquals("2=-1=0", decimalToSnafu(4890))
+        assertEquals("2011-=2=-1020-1===-1", decimalToSnafu(39021690220321))
     }
 }
